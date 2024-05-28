891b45d5285e172260b0a5de3a0fcd0edcc778a6
Valtteri Ahola <valtteri.ahola@helsinki.fi>
2024-05-28 19:53:35 +0300

    APIView for creating a new event

--- backend/calendar_app/urls.py
+++ backend/calendar_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+    path("events/create/", CreateNewEventView.as_view())
 ]
--- backend/calendar_app/views.py
+++ backend/calendar_app/views.py
@@ -10,3 +10,16 @@ class EventView(viewsets.ModelViewSet):
 
     serializer_class = EventSerializer
     queryset = Event.objects.all()
+
+class CreateNewEventView(APIView):
+    '''Create a new event'''
+
+    #permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        serializer = EventSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
